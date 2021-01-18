58a646def1b4383ccb77e56b93087840ff5452e2
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-01-18 17:10:53 +0800

    fetch network or client depending on role

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,10 @@ class PagesController < ApplicationController
   end
 
   def dashboard
+    if current_user.manager?
+      @network = current_user.manager.network
+    else
+      @clients = current_user.profile.managers
+    end
   end
 end
