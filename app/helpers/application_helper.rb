23f3837fd6420cdd8032fbd5fc6a02f6e288e043
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-01-18 17:29:31 +0800

    add helper to show links depending on role

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,34 @@
 module ApplicationHelper
+  SHARED_NAVBAR_LINKS = [
+    {
+      text: "Dashboard",
+      icon: "icons/dashboard"
+    }
+  ]
+  MANAGER_NAVBAR_LINKS = [
+    {
+      text: "Schedule",
+      icon: "icons/schedule"
+    },
+    {
+      text: "Freelancers",
+      icon: "icons/people"
+    }
+  ].freeze
+  FREELANCER_NAVBAR_LINKS = [
+    {
+      text: "Clients",
+      icon: "icons/people"
+    },
+    {
+      text: "Gigs",
+      icon: "icons/gigs"
+    }
+  ].freeze
+
+  def navbar_links(user)
+    links = user.manager? ? MANAGER_NAVBAR_LINKS : FREELANCER_NAVBAR_LINKS
+
+    [*SHARED_NAVBAR_LINKS, *links]
+  end
 end
