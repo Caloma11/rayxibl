c2d6ce9786ea8b6203c0fb957f02fbd283ce374f
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-01-19 14:16:53 +0800

    Job has one company via manager

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,6 +2,7 @@ class Job < ApplicationRecord
   acts_as_taggable_on :skills
 
   belongs_to :manager
+  has_one :company, through: :manager
   has_many :job_applications
 
   validates :location, :profession, :expertise, :description, :rate, :expiration_date, presence: true
