90d6f3ee95fe784dbcbe2ede75bc52c265503c0d
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-01-20 15:01:04 +0800

    convo has_many messages & method to retrieve other user

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,6 +1,11 @@
 class Conversation < ApplicationRecord
   belongs_to :manager
   belongs_to :profile
+  has_many :messages
 
   validates :manager, uniqueness: { scope: :profile }
+
+  def other_person(user)
+    @other_person ||= user == manager.user ? profile.user : manager.user
+  end
 end
