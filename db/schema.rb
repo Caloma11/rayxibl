168c556b311f3cd12affd4eb3cd0a5d1e40b8c5f
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-01-20 15:39:53 +0800

    add Booking#status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_19_044113) do
+ActiveRecord::Schema.define(version: 2021_01_20_073904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2021_01_19_044113) do
     t.integer "price_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status", default: 0
     t.index ["manager_id"], name: "index_bookings_on_manager_id"
     t.index ["profile_id"], name: "index_bookings_on_profile_id"
   end
