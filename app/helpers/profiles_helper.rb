7f1ea3ebc5546ab0e85c5fa6c6ccd44d41b6c9bb
gianluca perrone <perronegian@hotmail.com>
2021-01-21 12:40:09 +0800

    helper for avatar url

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,2 +1,10 @@
 module ProfilesHelper
+
+  def avatar_url(profile)
+    if profile.user.avatar.attached?
+      profile.user.avatar.service_url
+    else
+      "https://png.pngtree.com/png-clipart/20190520/original/pngtree-vector-users-icon-png-image_4144740.jpg"
+    end
+  end
 end
