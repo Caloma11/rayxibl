810c9744fc039e6e18c528dece811f2b5b8efdd5
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-01-22 14:44:49 +0800

    add attachments to Job

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,6 +4,7 @@ class Job < ApplicationRecord
   belongs_to :manager
   has_one :company, through: :manager
   has_many :job_applications
+  has_many_attached :attachments
 
   validates :location, :profession, :expertise, :description, :rate, :expiration_date, presence: true
 
