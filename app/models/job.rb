f4ccb2fc6bd3e737499622726f942eb5957a09f2
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-01-22 15:29:42 +0800

    add enum to Job

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,11 @@
 class Job < ApplicationRecord
+  include Bookings::Constants
+  include Profiles::Constants
+
   acts_as_taggable_on :skills
+
+  enum price_type: PRICE_TYPES
+  enum expertise: EXPERTISES
 
   belongs_to :manager
   has_one :company, through: :manager
