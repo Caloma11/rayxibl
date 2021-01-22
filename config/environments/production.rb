36dfa2a55a253f5da05bed2f15ba48f5847f8ec4
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-01-22 12:51:52 +0800

    https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
 
-  config.action_mailer.default_url_options = { host: "flxibl-staging.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "flxibl-staging.herokuapp.com", protocol: "https" }
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
