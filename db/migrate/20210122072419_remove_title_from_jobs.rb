6bf1ece4e43b07fedf618250030a29616b338120
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-01-22 15:29:13 +0800

    add columns to jobs

---
+++ db/migrate/20210122072419_remove_title_from_jobs.rb
@@ -0,0 +1,5 @@
+class RemoveTitleFromJobs < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :jobs, :title, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_22_063906) do
+ActiveRecord::Schema.define(version: 2021_01_22_072419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,8 +101,6 @@ ActiveRecord::Schema.define(version: 2021_01_22_063906) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "expertise"
-    t.string "title"
-    t.integer "experience_level"
     t.datetime "start_date"
     t.datetime "end_date"
     t.integer "duration"
