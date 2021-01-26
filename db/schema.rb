895170cbcc9a03584822c1275f1fd180a4570d8f
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-01-26 10:29:00 +0800

    add Job#status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_22_072419) do
+ActiveRecord::Schema.define(version: 2021_01_26_022837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(version: 2021_01_22_072419) do
     t.time "start_time"
     t.time "end_time"
     t.integer "price_type"
+    t.integer "status", default: 0
     t.index ["manager_id"], name: "index_jobs_on_manager_id"
   end
 
