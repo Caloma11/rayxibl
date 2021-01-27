dd1676a1889050d85ca74030b1aa1c2d9994a6b5
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-01-27 14:23:31 +0800

    include & serialize bookings

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,4 +1,5 @@
 class ProfileSerializer < ActiveModel::Serializer
   attributes :id, :avatar, :profession, :location
   attribute :display_name, key: :displayName
+  has_many :bookings, each_serializer: BookingSerializer
 end
