ea21f499974c5602e125eb2aa7c78667623a69d1
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-01-28 13:03:57 +0800

    JS to toggle steps

---
+++ app/javascript/components/stepForm.js
@@ -0,0 +1,53 @@
+const previousStepListen = ({
+	previousStepButton,
+	stepOne,
+	stepTwo,
+	stepIndicatorOne,
+	stepIndicatorTwo
+}) => {
+	previousStepButton.addEventListener("click", () => {
+		stepOne.classList.remove("none");
+		stepTwo.classList.add("none");
+		stepIndicatorOne.classList.add("active");
+		stepIndicatorTwo.classList.remove("active");
+	});
+};
+
+const nextStepListen = ({
+	nextStepButton,
+	stepOne,
+	stepTwo,
+	stepIndicatorOne,
+	stepIndicatorTwo
+}) => {
+	nextStepButton.addEventListener("click", () => {
+		stepOne.classList.add("none");
+		stepTwo.classList.remove("none");
+		stepIndicatorOne.classList.remove("active");
+		stepIndicatorTwo.classList.add("active");
+	});
+};
+
+export const stepForm = () => {
+	const container = document.querySelector(".manager-new-page");
+	const stepOne = document.querySelector(".step-1");
+	const stepTwo = document.querySelector(".step-2");
+	const previousStepButton = document.querySelector(".previous-step");
+	const nextStepButton = document.querySelector(".next-step");
+	const stepIndicatorOne = document.querySelector(".step-indicator-1");
+	const stepIndicatorTwo = document.querySelector(".step-indicator-2");
+
+	if (container) {
+		const elements = {
+			stepOne,
+			stepTwo,
+			nextStepButton,
+			previousStepButton,
+			stepIndicatorOne,
+			stepIndicatorTwo
+		};
+
+		nextStepListen(elements);
+		previousStepListen(elements);
+	}
+};
