654bfa478fc11a435716f09fbeeb596c4bd45fe8
gianluca perrone <perronegian@hotmail.com>
2021-01-29 15:19:44 +0800

    stopped propagation and added visual feedback

--- app/javascript/components/moreToggler.js
+++ app/javascript/components/moreToggler.js
@@ -3,8 +3,20 @@ export const moreToggler = () => {
 	const actionsContainer = document.querySelector(".actions-container");
 
 	if (trigger && actionsContainer) {
-		trigger.addEventListener("click", () => {
+		trigger.addEventListener("click", (e) => {
 			actionsContainer.classList.toggle("none");
+      e.stopPropagation();
 		});
+
+    actionsContainer.addEventListener("click", (e) => {
+      e.stopPropagation();
+    });
+
+    document.body.addEventListener('click', (e) => {
+      if(!actionsContainer.classList.contains('none')) {
+          actionsContainer.classList.add("none");
+      }
+    });
 	}
+
 };
