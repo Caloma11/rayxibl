28793dfb2f1412a992d576e66675d20c28706fdb
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-01-29 17:43:00 +0800

    click on input if img is clicked

--- app/javascript/components/previewImage.js
+++ app/javascript/components/previewImage.js
@@ -1,21 +1,41 @@
+const attachPreviewer = (img, input) => {
+	input.addEventListener("change", e => {
+		const [file] = e.target.files;
+
+		if (file) {
+			const reader = new FileReader();
+			reader.onload = evt => {
+				img.src = evt.currentTarget.result;
+			};
+			reader.readAsDataURL(file);
+			img.classList.remove("initial");
+		}
+	});
+};
+
+const attachImageListener = (img, input) => {
+	img.addEventListener("click", () => {
+		input.click();
+	});
+};
+
 export const previewImage = () => {
-	const previewer = document.getElementById("photo-preview");
+	const avatarPreviewer = document.getElementById("avatar-preview");
+	const logoPreviewer = document.getElementById("logo-preview");
 	const userAvatarInput = document.getElementById(
 		"manager_user_attributes_avatar"
 	);
+	const companyLogoInput = document.getElementById(
+		"manager_company_attributes_logo"
+	);
 
-	if (previewer && userAvatarInput) {
-		userAvatarInput.addEventListener("change", e => {
-			const [file] = e.target.files;
+	if (userAvatarInput && avatarPreviewer) {
+		attachPreviewer(avatarPreviewer, userAvatarInput);
+		attachImageListener(avatarPreviewer, userAvatarInput);
+	}
 
-			if (file) {
-				const reader = new FileReader();
-				reader.onload = evt => {
-					previewer.src = evt.currentTarget.result;
-				};
-				reader.readAsDataURL(file);
-				previewer.classList.remove("initial");
-			}
-		});
+	if (logoPreviewer && companyLogoInput) {
+		attachPreviewer(logoPreviewer, companyLogoInput);
+		attachImageListener(logoPreviewer, companyLogoInput);
 	}
 };
