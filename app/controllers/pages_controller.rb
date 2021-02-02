4708e5a3f3e5118d626ab1b9e73af77717c36621
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-02-02 15:33:01 +0800

    booking card

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,8 @@ class PagesController < ApplicationController
   def dashboard
     if current_user.manager?
       @network = current_user.manager.network
+      @booked_profiles = current_user.manager.booked_profiles
+      @bookings = current_user.manager.bookings
     else
       @clients = current_user.profile.managers
     end
