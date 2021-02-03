326dee5f2019127c09ab2c55056eccc63527f2f3
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-02-03 17:24:47 +0800

    handle Booking input

--- app/javascript/packs/components/Input.jsx
+++ app/javascript/packs/components/Input.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
-export const Input = ({ name, label, value, setValue }) => {
+export const Input = ({ booking = false, name, label, value, setValue }) => {
+	const inputName = attr => (booking ? `booking[${attr}]` : `profile[${attr}]`);
+
 	return (
 		<div className="flex flex-column mb-3">
 			<label htmlFor={`profile_${name}`} className="textLightBlack mb-1">
@@ -10,7 +12,7 @@ export const Input = ({ name, label, value, setValue }) => {
 				type="text"
 				value={value}
 				onChange={e => setValue(e.target.value)}
-				name={`profile[${name}]`}
+				name={inputName(name)}
 				id={`profile_${name}`}
 			/>
 		</div>
