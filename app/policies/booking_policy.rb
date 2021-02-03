d14dffd29a0ff5dd432cda3d663bfa1a444cd7d8
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-02-03 14:34:02 +0800

    update booking policy

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -1,12 +1,12 @@
 class BookingPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.all
+      scope
     end
   end
 
   def show?
-    record.manager == user.manager
+    record.manager == user.manager || record.profile == user.profile
   end
 
   def create?
