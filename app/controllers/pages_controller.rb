7f0c59c69fd2af43a1b843778a25ad3495635c1b
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-02-04 16:20:52 +0800

    text of amount of freelancers booked today

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,7 @@ class PagesController < ApplicationController
     if current_user.manager?
       @network = current_user.manager.network
       @booked_profiles = current_user.manager.booked_profiles
+      @today_booked_profiles = @booked_profiles.has_bookings_today
       @bookings = current_user.manager.bookings
       @jobs = current_user.manager.company.jobs.includes(:company, :manager).limit(3)
     else
