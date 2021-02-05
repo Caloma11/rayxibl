7c77ddc7651acca6795cd7e2edf11bd244108195
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-02-05 15:40:24 +0800

    new Convo scope

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,15 +1,21 @@
 class Conversation < ApplicationRecord
   belongs_to :manager
   belongs_to :profile
-  has_many :messages
+  has_many :messages, dependent: :destroy
 
   validates :manager, uniqueness: { scope: :profile }
 
+  # TODO: Find a way to do with only 1 scope that doesn't remove `Conversation` without messages
   scope :by_latest_message, -> {
     joins(:messages)
     .includes([:messages, profile: { user: :avatar_attachment }])
     .order("messages.created_at")
     .distinct
+  }
+  scope :with_no_messages, -> {
+    left_outer_joins(:messages)
+     .includes([:messages, profile: { user: :avatar_attachment }])
+     .distinct
   }
 
   def other_person(user)
