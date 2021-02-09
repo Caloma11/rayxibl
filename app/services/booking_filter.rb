580bd950ea283ee99b0363722b82a74c6de4f90b
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-02-09 13:05:29 +0800

    filter via location and clear filter

--- app/services/booking_filter.rb
+++ app/services/booking_filter.rb
@@ -16,6 +16,18 @@ class BookingFilter
       @bookings = @bookings.joins(:profile).where("profiles.profession ILIKE :profession", profession: "%#{params[:profession]}%")
     end
 
+    if params[:location] != ""
+      @bookings = @bookings.joins(:profile).where("profiles.location ILIKE :location", location: "%#{params[:location]}%")
+    end
+
+    if params[:clear] == "true"
+      if current_user.manager?
+        @bookings = current_user.manager.bookings.includes(profile: [user: { avatar_attachment: :blob }])
+      else
+        @bookings = current_user.profile.bookings
+      end
+    end
+
     @bookings
   end
 end
