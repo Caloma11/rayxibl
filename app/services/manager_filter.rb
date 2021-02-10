91c8331e6e23c60dac8a86ca4a739866e0d26946
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-02-10 13:04:46 +0800

    filter via job title

--- app/services/manager_filter.rb
+++ app/services/manager_filter.rb
@@ -1,6 +1,8 @@
 class ManagerFilter
   attr_reader :managers, :params, :current_user
 
+  NAME_QUERY = "users.first_name ILIKE :name OR users.last_name ILIKE :name OR companies.name ILIKE :name"
+
   def initialize(managers, params, current_user)
     @managers = managers
     @params = params
@@ -9,7 +11,13 @@ class ManagerFilter
 
   def call
     if params[:name]
-      @managers = @managers.joins(:company, :user).where("users.first_name ILIKE :name OR users.last_name ILIKE :name OR companies.name ILIKE :name", name: "%#{params[:name]}%")
+      @managers = @managers
+                    .joins(:company, :user)
+                    .where(NAME_QUERY, name: "%#{params[:name]}%")
+    end
+
+    if params[:job_title]
+      @managers = @managers.where("job_title ILIKE :title", title: "%#{params[:job_title]}%")
     end
 
     @managers
