7706f42dde06a8ab96b0e359671bca17940528b8
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-02-10 15:33:41 +0800

    reusable modal + prettier

--- app/javascript/components/editLinksAndDocuments.js
+++ app/javascript/components/editLinksAndDocuments.js
@@ -1,39 +1,38 @@
 const initLinksForm = () => {
-  const modalWrapper = document.getElementById("links-modal");
-  const addALink = document.getElementById("add-a-link");
-  const closeLinksModal = document.getElementById("close-links");
+	const modalWrapper = document.getElementById("links-modal");
+	const addALink = document.getElementById("add-a-link");
+	const closeLinksModal = document.getElementById("close-links");
 
+	if (addALink) {
+		addALink.addEventListener("click", e => {
+			modalWrapper.classList.remove("none");
+		});
 
-  if (addALink) {
-    addALink.addEventListener('click', (e) => {
-      modalWrapper.classList.remove("none");
-    })
-
-    closeLinksModal.addEventListener('click', (e) => {
-      modalWrapper.classList.add("none");
-    })
-  }
-}
+		closeLinksModal.addEventListener("click", e => {
+			modalWrapper.classList.add("none");
+		});
+	}
+};
 
 const initDocumentsForm = () => {
-  const modalWrapper = document.getElementById("documents-modal");
-  const uploadADocument = document.getElementById("upload-a-document");
-  const closeLinksModal = document.getElementById("close-documents");
+	const modalWrapper = document.getElementById("documents-modal");
+	const uploadADocument = document.getElementById("upload-a-document");
+	const closeLinksModal = document.getElementById("close-documents");
 
-  if (uploadADocument) {
-    uploadADocument.addEventListener('click', (e) => {
-      modalWrapper.classList.remove("none");
-    })
+	if (uploadADocument) {
+		uploadADocument.addEventListener("click", e => {
+			modalWrapper.classList.remove("none");
+		});
 
-    closeLinksModal.addEventListener('click', (e) => {
-      modalWrapper.classList.add("none");
-    })
-  }
-}
+		closeLinksModal.addEventListener("click", e => {
+			modalWrapper.classList.add("none");
+		});
+	}
+};
 
 const initEditLinksAndDocuments = () => {
-  initLinksForm();
-  initDocumentsForm();
-}
+	initLinksForm();
+	initDocumentsForm();
+};
 
-export { initEditLinksAndDocuments }
+export { initEditLinksAndDocuments };
