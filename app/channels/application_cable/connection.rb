18ceab93fc67eca384cb33ed4eca8ca3f4c10ccf
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-02-17 15:21:01 +0800

    remove current_user getter

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,25 +1,5 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    identified_by :current_user
-
-    def connect
-      self.current_user = find_verified_user
-    end
-
-    private
-
-    def find_verified_user
-      verified_id = session['warden.user.user.key'].try(:[], 0).try(:[], 0)
-      if verified_user = User.find(verified_id)
-        verified_user
-      else
-        reject_unauthorized_connection
-      end
-    end
-
-    def session
-      cookies.encrypted[Rails.application.config.session_options[:key]]
-    end
   end
 end
 #
