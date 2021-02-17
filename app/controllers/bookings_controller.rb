3b6b613ae2ba813b12d9bd2d708c773c8c089c97
gianluca perrone <perronegian@hotmail.com>
2021-02-17 13:05:22 +0800

    remove irrelevant comment"

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -42,7 +42,6 @@ class BookingsController < ApplicationController
     @profile = Profile.find(params[:profile_id])
     @booking.profile = @profile
     authorize @booking
-    # CALCULATE TOTAL_PRICE HERE
     if @booking.save
       redirect_to booking_path(@booking)
     else
