1ec3273aa1f452bcc9565d90288aeb313ea3c37c
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-02-17 10:38:46 +0800

    add validation for dates

--- app/javascript/components/error.js
+++ app/javascript/components/error.js
@@ -4,6 +4,12 @@ export const error = () => {
 	errorSpans.forEach(span => {
 		const input = span.previousElementSibling;
 
+		if (input.type === "hidden" && /_date/g.test(input.id)) {
+			const datepicker = document.querySelector(".datepicker.form-control");
+
+			datepicker.classList.add("error");
+		}
+
 		if (input) {
 			span.classList.add("none");
 			input.classList.add("error");
