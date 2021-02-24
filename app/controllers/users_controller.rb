6eb8fcb89f7830462c197c99f47e63edb3c0e6f4
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-02-24 16:04:57 +0800

    edit/update password controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,25 @@ class UsersController < ApplicationController
       redirect_to root_path
     end
   end
+
+  def edit_password
+    @user = current_user
+    authorize @user
+  end
+
+  def update_password
+    authorize current_user
+
+    if current_user.update(edit_password_params)
+      flash[:notice] = "Password successfully updated."
+      sign_in current_user, bypass: true
+      redirect_to settings_path
+    end
+  end
+
+  private
+
+  def edit_password_params
+    params.require(:user).permit(:password)
+  end
 end
