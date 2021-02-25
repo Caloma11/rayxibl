15d88394d8e666b8bfb52fd3928bd70e75c83baa
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-02-25 15:35:41 +0800

    switch to exchangerate-api

--- app/services/exchange_rate.rb
+++ app/services/exchange_rate.rb
@@ -1,15 +1,17 @@
 class ExchangeRate
   attr_reader :preferred_currency
 
+  include Users::Constants
+
   def initialize(user)
     @user = user
     @preferred_currency = user.preferred_currency.to_sym
+    @credentials = Rails.application.credentials.exchange_rate[:api_key]
   end
 
   def call
-    response = RestClient.get("https://api.exchangeratesapi.io/latest?base=USD&symbols=USD,GBP,EUR")
+    response = RestClient.get("https://v6.exchangerate-api.com/v6/#{@credentials}/latest/USD")
     data = JSON.parse(response).deep_transform_keys(&:to_sym)
-
-    data[:rates][preferred_currency]
+    data[:conversion_rates][preferred_currency]
   end
 end
