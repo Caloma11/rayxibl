8bb900047a37996bd48ee25ab4b1c2e3ef48f195
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-02-26 14:45:47 +0800

    Company 1:n job_applications through jobs

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,6 +2,7 @@ class Company < ApplicationRecord
   has_one_attached :logo
   has_many :managers, dependent: :destroy
   has_many :jobs, through: :managers
+  has_many :job_applications, through: :jobs
   has_many :notes, through: :managers
   has_many :connections
   has_many :profiles, through: :connections
