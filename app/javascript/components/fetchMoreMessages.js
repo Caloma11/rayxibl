5f292958d7e0117621a625bc8dda5d205c003201
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-03-04 16:19:49 +0800

    loading & safe nav.

--- app/javascript/components/fetchMoreMessages.js
+++ app/javascript/components/fetchMoreMessages.js
@@ -4,8 +4,10 @@ export const fetchMoreMessages = () => {
 	const form = document.getElementById("fetch-more-messages");
 
 	if (form) {
+		const fetchingIndicator = document.getElementById("fetching-indicator");
 		window.addEventListener("scroll", () => {
 			if (window.scrollY === 0) {
+				fetchingIndicator.classList.remove("none");
 				Rails.fire(form, "submit");
 			}
 		});
