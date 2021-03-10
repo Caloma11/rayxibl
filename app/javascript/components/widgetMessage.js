65c2f17ed53160498e2b39800945739069b4e1a4
gianluca perrone <perronegian@hotmail.com>
2021-03-10 17:38:52 +0800

    go back to previous logic where it is not possible to show keyboard straight away

--- app/javascript/components/widgetMessage.js
+++ app/javascript/components/widgetMessage.js
@@ -3,6 +3,19 @@ const initPrefill = () => {
 
   if (convoShow) {
     const input = document.getElementById("message_content");
+
+      console.log(input.value)
+
+    if (input.value !== "") {
+        const text = input.value;
+        input.value = "";
+        input.focus();
+        input.click();
+        input.value = text;
+        input.scrollLeft = input.scrollWidth;
+    }
+
+
     document.querySelectorAll(".widget-message").forEach((wm) => {
       wm.addEventListener("click", (e) => {
         e.preventDefault();
