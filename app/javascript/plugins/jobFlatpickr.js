8d9c4864b8b5a6beac36d7c459ee4811409fd591
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-03-10 12:40:15 +0800

    update new job time

--- app/javascript/plugins/jobFlatpickr.js
+++ app/javascript/plugins/jobFlatpickr.js
@@ -8,26 +8,50 @@ const handleChange = (dates, { start, end }) => {
 };
 
 export const jobFlatpickr = () => {
-	const startTimeInput = document.getElementById("job_start_time");
-	const endTimeInput = document.getElementById("job_end_time");
 	const dateInput = document.getElementById("job-datepicker");
 	const startDateInput = document.getElementById("job_start_date");
 	const endDateInput = document.getElementById("job_end_date");
 	const expirationDateInput = document.getElementById("job_expiration_date");
+	const jobTime = document.getElementById("job_time");
+	const timeWrapper = document.querySelector(".bookingSpecific");
+
 	const timepickerOptions = {
 		disableMobile: true,
 		noCalendar: true,
 		enableTime: true,
-		time_24hr: true
+		time_24hr: true,
+		positionElement: timeWrapper
 	};
+	let value = "";
 
-	if (startTimeInput) {
-		flatpickr(startTimeInput, timepickerOptions);
-	}
+	const endTimeFlat = flatpickr(".end-timepicker", {
+		...timepickerOptions,
+		onClose: (_, time) => {
+			value += ` - ${time}`;
+			jobTime.value = value;
+		}
+	});
 
-	if (endTimeInput) {
-		flatpickr(endTimeInput, timepickerOptions);
-	}
+	const startTimeFlat = flatpickr(".start-timepicker", {
+		...timepickerOptions,
+		onClose: () => endTimeFlat.open(),
+		onChange: (_, time) => {
+			value = time;
+		}
+	});
+
+	jobTime.addEventListener("click", e => {
+		e.preventDefault();
+		if (!startTimeFlat.isOpen && !endTimeFlat.isOpen) {
+			startTimeFlat.open();
+		} else if (startTimeFlat.isOpen) {
+			startTimeFlat.close();
+			endTimeFlat.open();
+		} else {
+			endTimeFlat.close();
+			startTimeFlat.open();
+		}
+	});
 
 	if (dateInput) {
 		flatpickr(dateInput, {
