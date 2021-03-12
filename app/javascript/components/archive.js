e3dc7b213869879c5d709b34bd982c4c0f921df2
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-03-12 15:45:15 +0800

    fix js error

--- app/javascript/components/archive.js
+++ app/javascript/components/archive.js
@@ -1,9 +1,9 @@
 export const archive = () => {
 	const toggler = document.getElementById("archive-toggler");
-	const togglerImg = toggler.querySelector("img");
 	const content = document.getElementById("archived-bookings");
 
 	if (toggler && content) {
+		const togglerImg = toggler.querySelector("img");
 		toggler.addEventListener("click", () => {
 			togglerImg.classList.toggle("open");
 			content.classList.toggle("open");
