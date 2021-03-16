62fb4f21571dd5bb228d201de8c5404fb52e9dcf
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-03-16 15:05:38 +0800

    custom `bookings` method

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,5 +1,11 @@
 class ProfileSerializer < ActiveModel::Serializer
   attributes :id, :avatar, :profession, :location
   attribute :display_name, key: :displayName
-  has_many :bookings, each_serializer: BookingSerializer
+  attribute :bookings
+
+  def bookings
+    object.bookings.where(manager_id: @instance_options[:manager_id]).map do |booking|
+      BookingSerializer.new(booking, scope: scope, root: false)
+    end
+  end
 end
