dfd6257d0c6e713373f675e5703269fe95f173bb
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-03-16 17:00:54 +0800

    new booking title

--- app/javascript/packs/components/BookingFormHeader.jsx
+++ app/javascript/packs/components/BookingFormHeader.jsx
@@ -6,21 +6,23 @@ export const BookingFormHeader = ({ setShowForm, profile }) => {
 		<div className="booking-form-header">
 			<div className="flex justify-content-between">
 				<button type="button" onClick={() => setShowForm(false)}>
-					<img src={blackCrossSvg} alt="back" width={16} height={16} />
+					<img src={blackCrossSvg} alt="back" width={14} height={14} />
 				</button>
-				<h3>New booking</h3>
+				<h3 className="semi-bold uppercase textBetween textDarkGray">
+					New booking
+				</h3>
 				<div style={{ width: 28 }}></div>
 			</div>
 
 			<div className="flex items-center profile">
 				<img
-					className="avatar avatar-square"
+					className="avatar avatar-square medium"
 					src={profile.avatar}
 					alt={`${profile.displayName}'s avatar`}
 				/>
 				<div className="ml-3">
 					<p className="mt-0 mb-1">{profile.displayName}</p>
-					<span>
+					<span className="textBetween">
 						{profile.profession}, {profile.location}
 					</span>
 				</div>
