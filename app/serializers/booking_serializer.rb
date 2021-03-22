79abd03954b13c0fc65ceb6c66eceac20cc17018
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2021-03-22 12:41:02 +0800

    expose Booking#weekends & Booking#duration_in_hours for API

--- app/serializers/booking_serializer.rb
+++ app/serializers/booking_serializer.rb
@@ -1,7 +1,9 @@
 class BookingSerializer < ActiveModel::Serializer
-  attributes :id, :title, :status, :profile_id, :status, :duration
+  attributes :id, :title, :status, :profile_id, :status, :duration, :weekends
   %w[start end].each do |identifier|
     attribute :"parsed_#{identifier}_date", key: :"#{identifier}Date"
     attribute :"parsed_#{identifier}_time", key: :"#{identifier}Time"
   end
+
+  attribute :duration_in_hours, key: :durationInHours
 end
